8f19c0d9e33f818858d6470642493d1f05de7ec0
mnik01 <39025497+mnik01@users.noreply.github.com>
2023-02-25 00:16:38 +0600

    Select row inside of a table added

--- src/lib/Components/BaseTable/BaseTable.tsx
+++ src/lib/Components/BaseTable/BaseTable.tsx
@@ -24,16 +24,31 @@ export const BaseTable = <T extends { id: string }>({
     fetchTableData();
   }, [sorting, pageIndex, pageSize]);
 
+  const onRowClick = (row: T) => {
+    dispatch({ type: TableStateActions.SET_SELECTED_ROW, payload: row });
+  };
+
   return (
     <div>
       {isLoading && <h1>LOADING...</h1>}
       {!isLoading && (
-        <span className="text-xs">User id: {tableData.query[0].id}</span>
+        <div className="w-full bg-blue-100 divide-y-2 divide-black">
+          {tableData.query.map((row) => (
+            <div
+              onClick={() => {
+                onRowClick(row);
+              }}
+              className="text-xs cursor-pointer p-2 hover:bg-blue-200"
+            >
+              row id: {row.id}
+            </div>
+          ))}
+        </div>
       )}
       {selectedRow ? (
         <p>selected: {selectedRow.id}</p>
       ) : (
-        <p>there is no selected row</p>
+        <p>click to select row</p>
       )}
     </div>
   );
